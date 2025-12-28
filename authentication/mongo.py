c27a737e5cb57317644f1b22d5aeba3728021eb9
Mukesh <mukidvm@gmail.com>
2025-12-28 22:15:55 +0530

    Update mongo.py

--- authentication/mongo.py
+++ authentication/mongo.py
@@ -4,8 +4,9 @@ from pymongo import MongoClient
 MONGO_URI = os.environ.get("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
-db = client["no_due_db"]
+db = client["no_dues_portal"]
 # Collections (future use)
 institution_logs = db["institution_logs"]
 no_due_col = db["no_due_requests"]
 students_col = db["students"]
+
